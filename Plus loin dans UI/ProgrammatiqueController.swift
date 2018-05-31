e3e77cc7a39b88bd939482486f89691007ac7ea9
Snoopy <snoopy@MacBook-Pro-de-SERGENT.local>
2018-05-31 14:35:26 +0200

    UILabel

--- Plus loin dans UI/ProgrammatiqueController.swift
+++ Plus loin dans UI/ProgrammatiqueController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ProgrammatiqueController: UIViewController {
     
     var  monPremierUIView: UIView?
-    
+    var monPremierLabel : UILabel?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,19 +19,14 @@ class ProgrammatiqueController: UIViewController {
         monPremierUIView?.backgroundColor = UIColor(red: 30/255, green: 123/255, blue: 90/255, alpha: 1)
         monPremierUIView?.layer.cornerRadius = 25
         view.addSubview(monPremierUIView!)
-        print(monPremierUIView!.frame)
-        print(monPremierUIView!.bounds)
         
-        let secondeVue = UIView(frame: monPremierUIView!.frame)
-        secondeVue.center.y+=100
-        secondeVue.backgroundColor = .red
-        view.addSubview(secondeVue)
-        
-        let troisiemeVue = UIView(frame: monPremierUIView!.bounds)
-        troisiemeVue.center.y += 200
-        troisiemeVue.backgroundColor = UIColor.brown
-        view.addSubview(troisiemeVue)
-        
+        monPremierLabel = UILabel(frame: CGRect(x: 0, y: monPremierUIView!.frame.maxY + 20, width: view.frame.width, height: 50))
+        monPremierLabel?.text = "Coucou je suis un texte"
+        monPremierLabel?.numberOfLines = 1
+        monPremierLabel?.textColor = UIColor.white
+        monPremierLabel?.font = UIFont(name: "Chalkduster", size: 20)
+        monPremierLabel?.textAlignment = .left
+        view.addSubview(monPremierLabel!)
     }
 
 }
